3cb6057040daad7368cab800681d1e0aca84dcf7
常見夏希 <tsuneminatsuki@tsuneminatsukinoMacBook-Air.local>
2020-09-23 16:07:17 +0900

    [add] Const.java

---
+++ Const.java
@@ -0,0 +1,12 @@
+class Const {
+    public static void main(String[] args){
+        final double BASE_LEN = 18.29;
+        System.out.println("ソフトボールは"+7+"イニングまでです。");
+        System.out.println("類間は"+BASE_LEN+"mです。");
+        System.out.println('あ');
+        System.out.println('\u0041');
+        System.out.println(true);
+        System.out.println(0b1010);
+    }
+    
+}
