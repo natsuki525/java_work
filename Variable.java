2bf4125b68496c46191fc193a276daacd16fd40a
常見夏希 <tsuneminatsuki@tsuneminatsukinoMacBook-Air.local>
2020-09-23 14:34:39 +0900

    [add] Variabel.java

---
+++ Variable.java
@@ -0,0 +1,11 @@
+class Variable {
+    public static void main(String[] args){
+        int diameter=10;
+        int radius=5;
+        double area;
+
+        System.out.println("直径は"+diameter);
+        System.out.println("半径は"+radius);
+        System.out.println("円の面積は"+radius*radius*3.14);
+    }
+}
