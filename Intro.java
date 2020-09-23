65afce0a02e2e987327e93b17a4a5db2e2e4bb14
常見夏希 <tsuneminatsuki@tsuneminatsukinoMacBook-Air.local>
2020-09-23 14:08:40 +0900

    [add] introduction

---
+++ Intro.java
@@ -0,0 +1,9 @@
+class Intro{
+    public static void main(String[] args){
+        System.out.println("私は内藤誠です。");
+        System.out.println("好きなスポーツはゴルフです。");
+        System.out.println("好きな食べ物はタコスです。");
+        System.out.println("好きなアニメはラブライブです。");
+        System.out.println("一人前のSEになれるように修行中です。");
+    }
+}
