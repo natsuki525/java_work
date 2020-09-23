9a81a5d8640caadb0d5ef3eee09bafc9134d52d8
常見夏希 <tsuneminatsuki@tsuneminatsukinoMacBook-Air.local>
2020-09-23 14:45:45 +0900

    [add] VarString.java

---
+++ VarString.java
@@ -0,0 +1,10 @@
+class VarString{
+    public static void main(String[] args){
+        String name = "Moka";
+        int age = 17;
+        String address = "江東区";
+
+        System.out.println("My name is " + name +". I'm "+age+" years old.");
+        System.out.println("I live in "+address+".");
+    }
+}
