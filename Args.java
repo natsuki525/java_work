99e28378a0da0d57a8a6e5cf47d724d0035443d8
常見夏希 <tsuneminatsuki@tsuneminatsukinoMacBook-Air.local>
2020-09-24 14:37:16 +0900

    [add] Args.java

---
+++ Args.java
@@ -0,0 +1,12 @@
+class Args {
+    public static void main(String[] args){
+        String color = "green";
+        char moji = color.charAt(0);
+        int age = 21;
+
+
+        System.out.println("好きな色の先頭文字は "+moji);
+        System.out.println("好きな色は "+color);
+        System.out.println("10年後は "+(age+10)+"才");
+    }    
+}
